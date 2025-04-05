451d555fb0bb9c10bcb6bb1051b02f629447cbec
siwan9 <si.wan208@gmail.com>
2025-04-05 13:29:41 +0900

    chore: 자잘한 수정

--- application/yabam/src/main/java/com/application/festival/yabam/HelloController.java
+++ application/yabam/src/main/java/com/application/festival/yabam/HelloController.java
@@ -1,16 +1,26 @@
 package com.application.festival.yabam;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.application.global.authorization.AssignUserPassport;
+import com.application.global.authorization.HasRole;
+
+import domain.pos.member.entity.UserPassport;
+import domain.pos.member.entity.UserRole;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/yabam")
 public class HelloController {
 
 	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, Yabam!";
+	@HasRole(userRole = UserRole.ROLE_ANONYMOUS)
+	@AssignUserPassport
+	public ResponseEntity<Void> hello(UserPassport userPassport) {
+		return ResponseEntity.ok().build();
 	}
 }
--- domain/domain-pos/src/main/java/domain/pos/member/entity/UserRole.java
+++ domain/domain-pos/src/main/java/domain/pos/member/entity/UserRole.java
@@ -1,6 +1,11 @@
 package domain.pos.member.entity;
 
 public enum UserRole {
+	ROLE_ANONYMOUS,
 	ROLE_USER,
-	ROLE_OWNER
+	ROLE_OWNER;
+
+	public boolean isHigherOrEqual(UserRole other) {
+		return this.ordinal() >= other.ordinal();
+	}
 }
